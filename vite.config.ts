c22e4da6604674863f8187a6537c91d5f3952260
Daniel Sant'Anna <danielcarlossantanna@gmail.com>
2024-08-10 01:40:27 -0300

    feat: add capacitor to generate APK for Android

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import EnvironmentPlugin from "vite-plugin-environment";
 
+// Clear specific environment variables
+delete process.env["CommonProgramFiles(x86)"];
+delete process.env["ProgramFiles(x86)"];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin("all")],
